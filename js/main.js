2ce470616c4185ba760fff1e395f085a21b3a323
homerioni <homerion13@gmail.com>
2022-03-23 11:18:42 +0300

    create header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 'use strict'
 
 $(document).ready(function () {
+
+    // header функционал
+    $('.header__services-menu-name').click(function () {
+        $(this).toggleClass('open')
+            .parent().find('.header__services-list').slideToggle();
+    });
+    $('.header__services-item-name').click(function () {
+        $(this).toggleClass('open')
+            .parent().find('.header__services-sub-list').slideToggle();
+    });
+    $('.header__about-menu-btn > svg').click(function () {
+        $(this).parent().toggleClass('open')
+            .parent().find('.header__about-list').slideToggle();
+    });
+
 });
