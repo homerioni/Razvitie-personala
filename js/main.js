7cf0c5823eb3f6cc1d0749cbf20bada79510444a
homerioni <homerion13@gmail.com>
2022-03-31 19:59:11 +0300

    create services.html

--- js/main.js
+++ js/main.js
@@ -12,8 +12,6 @@ $(document).ready(function () {
             .parent().find('.header__services-sub-list').slideToggle();
     });
 
-    // main-intro анимация картинок
-
     // custom checkbox
     $('.label--checkbox').click(function () {
         if ($(this).find('input').prop('checked')) {
@@ -31,21 +29,23 @@ $(document).ready(function () {
     $('.rating-number').text(rating.slice(0, 1) + ',' + rating.slice(-1)); //Вставляю в текст оценку
     $('.rating-icons-full').css('width', Math.trunc(rating) * 20 + 4 + Number(rating.slice(-1)) / 10 * 12 + '%'); //Высчитываю ширину для коректного отображения звезд
 
-    // calculate cтилизация input-range
-    let input_range = $('.calculate__input--range');
-    $('.calculate__input-range-value').text(input_range.val());
+    // cтилизация input-range
+    let input_range = $('.input-range');
+    $('.range-min').text(input_range.attr('min'));
+    $('.range-max').text(input_range.attr('max'));
+    $('.input-range-value').text(input_range.val());
     input_range.css({
         '--value': input_range.val(),
         '--min': input_range.attr('min'),
         '--max': input_range.attr('max'),
     });
-    $(document).on('input change', '.calculate__input--range', function() {
+    $(document).on('input change', '.input-range', function() {
         $(this).css({
             '--value': $(this).val(),
             '--min': $(this).attr('min'),
             '--max': $(this).attr('max'),
         });
-        $('.calculate__input-range-value').text($(this).val());
+        $('.input-range-value').text($(this).val());
     });
 
     // about-content text limited
@@ -53,4 +53,24 @@ $(document).ready(function () {
         $(this).css('display', 'none')
             .parent().find('article').css('display', 'block');
     });
+
+    // services функционал левого меню
+    $('.services__services-item-name').click(function () {
+        $(this).toggleClass('open')
+            .parent().find('.services__services-sub-list').slideToggle();
+    });
+    if ($(document).width() <= 768) {
+        $('.services__services-menu-name').click(function () {
+            let this_btn = $(this);
+            if ($(this).hasClass('open')) {
+                $(this).parent().find('.services__services-list').slideUp(300);
+                setTimeout(function () {
+                    this_btn.removeClass('open');
+                }, 250);
+            } else {
+                $(this).addClass('open')
+                    .parent().find('.services__services-list').slideDown(300);
+            }
+        });
+    }
 });
