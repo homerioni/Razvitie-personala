88385eeb3cac5eafa997114076c41e8a339635dc
homerioni <homerion13@gmail.com>
2022-07-05 15:45:03 +0300

    up

--- js/main.js
+++ js/main.js
@@ -218,13 +218,8 @@ $(document).ready(function () {
     });
 
     // modal-cart
-    $('.modal-cart').css('display', 'flex').hide();
-    $('.buy').click(function () {
-        $('.modal-cart').fadeIn(200);
-        $('body').addClass('lock');
-    });
     $('.modal-cart .close').click(function () {
-        $('.modal-cart').fadeOut(150);
+        $('.modal-cart').hide();
         $('body').removeClass('lock');
     });
 
